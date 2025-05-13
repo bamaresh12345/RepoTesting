1b6cee9bdd0989453b0f3739dbaac0bbd7b166a0
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-13 16:19:52 -0400

    dev1

---
+++ gitFiles/gitTesting/src/main/java/PlayrightNewProject/App.java
@@ -0,0 +1,13 @@
+package PlayrightNewProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ gitFiles/gitTesting/src/test/java/Dev1.java
@@ -0,0 +1,6 @@
+public class Dev1 {
+
+    public static void main(String[] args) {
+        System.out.println("this is dev1 testing");
+    }
+}
