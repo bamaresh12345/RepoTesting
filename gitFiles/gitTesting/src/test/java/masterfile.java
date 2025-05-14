fe69d3cb4815bd9a30f18b1c4cb402134239a5db
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-13 23:08:59 -0400

    test

--- gitFiles/gitTesting/src/test/java/masterfile.java
+++ gitFiles/gitTesting/src/test/java/masterfile.java
@@ -1,6 +1,8 @@
 public class masterfile {
 
     public static void main(String[] args) {
-        System.out.println("master file");
+
+        System.out.println("master file  dev1");
+        //testing dev1
     }
 }
