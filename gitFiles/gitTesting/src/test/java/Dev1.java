00ab13c3b41ae377d91a6e416932430b83405afd
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-13 21:53:23 -0400

    comment2

--- gitFiles/gitTesting/src/test/java/Dev1.java
+++ gitFiles/gitTesting/src/test/java/Dev1.java
@@ -2,14 +2,10 @@ public class Dev1 {
 
     public static void main(String[] args) {
         System.out.println("this is dev1 testing");
-        System.out.println("this is dev1 testing");
-<<<<<<< HEAD
+        System.out.println
 
         //Comment2
 
 
-        //comment 3
-=======
->>>>>>> parent of 2d6ad1b (comment2)
     }
 }
