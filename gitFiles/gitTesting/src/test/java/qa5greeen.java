815a49c02a8cb0c59e5959294b2824a7ee09a6ad
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-13 22:48:24 -0400

    tst

--- gitFiles/gitTesting/src/test/java/qa5greeen.java
+++ gitFiles/gitTesting/src/test/java/qa5greeen.java
@@ -2,5 +2,6 @@ public class qa5greeen {
 
     public static void main(String[] args) {
         System.out.println("qa5 green");
+        System.out.println("qa5 green");
     }
 }
---
+++ gitFiles/gitTesting/src/test/java/qa6green2.java
@@ -0,0 +1,7 @@
+public class qa6green2 {
+
+    public static void main(String[] args) {
+        System.out.println("qa6");
+    }
+
+}
