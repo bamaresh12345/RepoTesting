522e1da52490080d90f32d7b7a1b475141de88fe
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-13 23:18:40 -0400

    sadfds

--- gitFiles/gitTesting/src/test/java/masterfile.java
+++ gitFiles/gitTesting/src/test/java/masterfile.java
@@ -2,7 +2,9 @@ public class masterfile {
 
     public static void main(String[] args) {
 
-        System.out.println("master file  dev1");
+        System.out.println("master file  dev2");  // dev2
         //testing dev1
+
+        System.out.println("dev2");
     }
 }
