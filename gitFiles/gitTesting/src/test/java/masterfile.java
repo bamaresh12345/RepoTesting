03dfe00b88b3849907ab506125d327651d9d5398
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-13 23:17:26 -0400

    sdfsd

--- gitFiles/gitTesting/src/test/java/masterfile.java
+++ gitFiles/gitTesting/src/test/java/masterfile.java
@@ -2,6 +2,8 @@ public class masterfile {
 
     public static void main(String[] args) {
 
-        System.out.println("master file  qa1");
+        System.out.println("master file  qa1"); //qa2
+
+        System.out.println("not changed in master"); // qa2 newly added
     }
 }
