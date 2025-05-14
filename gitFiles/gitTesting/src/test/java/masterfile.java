6be519a4757e7644b5ae716416374b802ab3f402
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-13 23:34:38 -0400

    asdfasdf

--- gitFiles/gitTesting/src/test/java/masterfile.java
+++ gitFiles/gitTesting/src/test/java/masterfile.java
@@ -6,7 +6,7 @@ public class masterfile {
         System.out.println("master file  qa1,q2  and dev1 , dev2");
 
         //asdfas
-        System.out.println("only qa3 and dev3");
+        System.out.println("only qa3 and dev3   dev4");
 
     }
 }
