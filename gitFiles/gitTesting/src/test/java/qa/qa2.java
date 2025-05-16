0f9463fd07230f2c677d0c87435e87b230ec1f88
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-16 13:36:53 -0400

    testng

---
+++ gitFiles/gitTesting/src/test/java/qa/qa2.java
@@ -0,0 +1,10 @@
+package qa;
+
+public class qa2 {
+    /*
+   git restore --stage gitTesting/src/test/java/qa/qa2.java   ---> --stage**** IMP to restore from staged (GREEN to RED)
+   git restore gitTesting/src/test/java/qa/qa2.java --> just to pull old changes into file, v1 version deatails
+   (use "git restore <file>..." to discard changes in working directory
+     stage 2 , to get stash of 1
+     */
+}
