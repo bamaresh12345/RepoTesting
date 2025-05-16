000c832577622154b11a7f4d93016a8e94fe6318
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-16 12:41:36 -0400

    asdfasdf

--- gitFiles/gitTesting/src/test/java/qa3.java
+++ gitFiles/gitTesting/src/test/java/qa3.java
@@ -2,4 +2,6 @@ public class qa3 {
     public static void main(String[] args) {
         System.out.println("QA 3");
     }
+
+    // this ofr tesintg qa1
 }
---
+++ gitFiles/gitTesting/src/test/java/test1/stash2.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class stash2 {
+
+    /*
+  stadh2 1
+
+   stadh2 2
+
+     */
+}
