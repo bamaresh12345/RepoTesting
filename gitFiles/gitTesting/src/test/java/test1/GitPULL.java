a3b37911b63a4cf73b2b5996b558f3d35392aeb2
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-18 10:44:07 -0400

    pull changes

---
+++ gitFiles/gitTesting/src/test/java/test1/GitPULL.java
@@ -0,0 +1,20 @@
+package test1;
+
+public class GitPULL {
+
+    /*
+       edit file in master and pull
+
+       edit multiple filea in master and pull
+
+       edit file in master and edit same file in local and pull
+       resolve merge conflit , keep my changes
+
+       edit file in master and edit same file in local and pull
+       resolve merge conflit , keep their changes
+
+       edit file in master and edit same file in local and pull
+       resolve merge conflit ,manual changes
+
+     */
+}
