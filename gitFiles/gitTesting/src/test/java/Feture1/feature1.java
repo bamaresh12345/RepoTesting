3b8bd3d53c84272ba1a3da946237722331ca6097
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-18 15:07:26 -0400

    asdf

--- gitFiles/gitTesting/src/test/java/Feture1/feature1.java
+++ gitFiles/gitTesting/src/test/java/Feture1/feature1.java
@@ -4,5 +4,11 @@ public class feature1 {
     /*
      Feature 1 added
 
+     Feature 2 added --> delete all files in feature 1 and veryify merge conflits
+     come back to command more and do
+     git merge master --> to get all deleted files
+     git add . ; git commit and git push
+     will resolve the conflits
+
      */
 }
