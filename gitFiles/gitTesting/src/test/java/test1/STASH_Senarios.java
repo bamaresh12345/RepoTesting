1dd3c7b04e06a8a485e50b5fa5e6f152b6f37e2f
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-18 11:06:23 -0400

    asdfasd

---
+++ gitFiles/gitTesting/src/test/java/test1/STASH_Senarios.java
@@ -0,0 +1,23 @@
+package test1;
+
+public class STASH_Senarios {
+
+    /*
+  stadh2 1
+
+   1. Stash in q1 , only add not commit else , cant pop
+    and checkout master , stash pop and move to fodelr and add,commit and push to master
+
+   2. Stash in q2 , only add not commit else , cant pop
+    and checkout master , stash pop and move to fodelr and add,commit and push to master
+
+    3. Stash in q1 , only add not commit else , cant pop
+    and checkout q2 , stash pop and move to fodelr and add,commit and push to q2
+    and merge to Master.
+
+     4. Stash in q2 , only add not commit else , cant pop
+    and checkout q1 , stash pop and move to fodelr and add,commit and push to q1
+    and merge to Master.
+
+     */
+}
