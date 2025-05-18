4f72b265c4a33463658374cd2439e121a07cfc7e
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-18 10:55:03 -0400

    asdfasd

---
+++ gitFiles/gitTesting/src/test/java/GIT/REST_Soft_Hard.java
@@ -0,0 +1,42 @@
+package GIT;
+/*
+git reset <commit> :	Moves the branch pointer to a specified commit, resetting the staging area and the working directory
+to match the specified commit.
+it retains the red color files in results
+
+git restore <file>
+git restore --staged <file>
+
+git reset --soft <commit> :	Moves the branch pointer to a specified commit, preserving the changes in the staging area and
+the working directory.
+it retains the greec color files in results
+
+git restore <file>
+git restore --staged <file>
+
+
+git reset --hard <commit> :	Moves the branch pointer to a specified commit, discarding all changes in the staging area and
+the working directory, effectively resetting the repository to the specified commit.
+
+it it directly deletes the contents of files in results , *** NOT recommend until you are sure about it.
+
+git restore <file>
+git restore --staged <file>
+
+ */
+public class REST_Soft_Hard {
+
+    /*                                git add   ; git commit -m "RESTORE testing 1"
+      RESTORE testing 1
+
+      RESTORE testing 2
+
+      RESTORE testing 3
+
+      RESTORE testing 4
+
+      RESTORE testing 5
+
+     */
+
+}
