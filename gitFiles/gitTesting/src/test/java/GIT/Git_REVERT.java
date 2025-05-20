a302840ced0c5ade793a21ca184a966d9962104f
bamaresh12345 <bamaresh2012@gmail.com>
2025-05-19 23:02:56 -0400

    asdfasdf

---
+++ gitFiles/gitTesting/src/test/java/GIT/Git_REVERT.java
@@ -0,0 +1,17 @@
+package GIT;
+
+public class Git_REVERT {
+
+    /*
+      1.  Make chaanges in sub-Branch and merge to master branch
+       in merge PR click on PR commit ,link and click on REVERT button
+       it will automatically will create an new branch with branchName-revert2
+       create a pull resuqest and merge it.
+
+       2. To again Revert BACK form Master branch PR ,
+       in merge PR click on PR commit ,link and click on REVERT button
+       it will automatically will create an new branch with branchName-revert2
+       create a pull resuqest and merge it.
+
+     */
+}
